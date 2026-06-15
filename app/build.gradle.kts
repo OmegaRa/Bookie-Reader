27168f75e0f4a2820752b02fd52d7305d28df5c8
Shaun Sommer <shaunsommer@outlook.com>
2026-06-15 15:25:14 -0400

    Update app version to 1.2.1 and increment versionCode to 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.bookiereader"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1.5"
+        versionCode = 7
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
