f213de08af29b9b6fdf448add9faff5c6f1f87ec
Shaun Sommer <shaunsommer@outlook.com>
2026-06-16 17:49:29 -0400

    Update app version to 1.2.3 and increment versionCode to 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.bookiereader"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.2.2"
+        versionCode = 9
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
